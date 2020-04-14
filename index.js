e1db0bdfce87c593dfa7cb2037d8940ef275cdfb
cyrilf <cyrilf7x@gmail.com>
2020-04-14 19:22:06 +0200

    Add a comment for right sidebar feature

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = {
-  extend: "@vuepress/theme-default",
+module.exports = (themeConfig) => {
+  // Will be useful when the right sidebar is developed.
+  // In Gitbook, sub-headers are in a right sidebar, so the left one shouldn't
+  // include them.
+  //
+  // themeConfig.sidebarDepth = themeConfig.sidebarDepth || 0
+
+  return {
+    extend: "@vuepress/theme-default",
+  }
 }
